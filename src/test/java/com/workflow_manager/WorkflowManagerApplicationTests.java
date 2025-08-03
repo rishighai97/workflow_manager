7af4cf5c9ac90ebdeffb2ab9145432e8982a9a00
Rishi Ghai <rishighai97@gmail.com>
2025-08-03 13:50:11 +0530

    first commit

---
+++ src/test/java/com/workflow_manager/WorkflowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
